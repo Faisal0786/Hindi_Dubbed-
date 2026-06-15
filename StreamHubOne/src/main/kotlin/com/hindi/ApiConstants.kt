f17075dd802c1b958649127cbe14b954f4d80b4a
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-06-16 04:20:20 +0530

    Create ApiConstants.kt

---
+++ StreamHubOne/src/main/kotlin/com/hindi/ApiConstants.kt
@@ -0,0 +1,34 @@
+package com.hindi
+
+object ApiConstants {
+
+    const val CINEMETA_API =
+        "https://v3-cinemeta.strem.io"
+
+    const val HAGLUND_API =
+        "https://arm.haglund.dev/api/v2"
+
+    const val ANILIST_API =
+        "https://graphql.anilist.co"
+
+    const val KITSU_API =
+        "https://anime-kitsu.strem.fun"
+
+    const val TMDB_API =
+        "https://api.themoviedb.org/3"
+
+    const val TMDB_IMAGE =
+        "https://image.tmdb.org/t/p"
+
+    const val TMDB_POSTER =
+        "$TMDB_IMAGE/w780"
+
+    const val TMDB_BACKDROP =
+        "$TMDB_IMAGE/original"
+
+    const val TMDB_LOGO =
+        "$TMDB_IMAGE/w500"
+
+    const val YOUTUBE_BASE =
+        "https://www.youtube.com/watch?v="
+}
