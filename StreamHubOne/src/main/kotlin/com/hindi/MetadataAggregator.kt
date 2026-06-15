6d7161ac99c65e2ed1cd2cad216fc63c96da3398
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-06-16 05:29:09 +0530

    Update MetadataAggregator.kt

--- StreamHubOne/src/main/kotlin/com/hindi/MetadataAggregator.kt
+++ StreamHubOne/src/main/kotlin/com/hindi/MetadataAggregator.kt
@@ -1,9 +1,7 @@
 Package com.hindi
 
 import com.lagradost.cloudstream3.app
-import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
 import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import java.net.URLEncoder
 
