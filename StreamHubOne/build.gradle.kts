e1ff5bf294d2ba5d89ed3a5edc0ba8a62a7c33b1
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-06-16 05:08:26 +0530

    Create build.gradle.kts

---
+++ StreamHubOne/build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+
+version = 1
+
+android {
+    defaultConfig {
+
+        val properties = Properties()
+        properties.load(
+            project.rootProject
+                .file("local.properties")
+                .inputStream()
+        )
+
+        buildFeatures.buildConfig = true
+
+        buildConfigField(
+            "String",
+            "TMDB_KEY",
+            "\"${properties.getProperty("TMDB_KEY")}\""
+        )
+    }
+}
+
+cloudstream {
+    language = "hi"
+
+    description =
+        "StreamHub One - Multilingual provider"
+
+    authors = listOf(
+        "Faisal"
+    )
+
+    status = 1
+
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime",
+        "AnimeMovie",
+        "AsianDrama"
+    )
+
+    iconUrl = ""
+}
