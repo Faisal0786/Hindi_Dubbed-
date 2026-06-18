052a76229c45c348bc8661913eff49766c5f3714
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-06-19 01:36:12 +0530

    Update SourceRegistry.kt

--- StreamHubOne/src/main/kotlin/com/hindi/providers/SourceRegistry.kt
+++ StreamHubOne/src/main/kotlin/com/hindi/providers/SourceRegistry.kt
@@ -22,9 +22,9 @@ data class SourceProviderDef(
     val key: String,
     val displayName: String,
     val isTorrent: Boolean = false,
-    val executeStandard: (suspend SourceProvider.(res: AllLoadLinksData, subCb: (SubtitleFile) -> Unit, cb: (ExtractorLink) -> Unit) -> Unit)? = null,
-    val executeAnime: (suspend SourceProvider.(res: AllLoadLinksData, subCb: (SubtitleFile) -> Unit, cb: (ExtractorLink) -> Unit) -> Unit)? = null,
-    val executeMalSync: (suspend SourceProvider.(data: MalSyncData, subCb: (SubtitleFile) -> Unit, cb: (ExtractorLink) -> Unit) -> Unit)? = null
+    val executeStandard: (suspend SourceProviders.(res: AllLoadLinksData, subCb: (SubtitleFile) -> Unit, cb: (ExtractorLink) -> Unit) -> Unit)? = null,
+    val executeAnime: (suspend SourceProviders.(res: AllLoadLinksData, subCb: (SubtitleFile) -> Unit, cb: (ExtractorLink) -> Unit) -> Unit)? = null,
+    val executeMalSync: (suspend SourceProviders.(data: MalSyncData, subCb: (SubtitleFile) -> Unit, cb: (ExtractorLink) -> Unit) -> Unit)? = null
 )
 
 object SourceRegistry {
