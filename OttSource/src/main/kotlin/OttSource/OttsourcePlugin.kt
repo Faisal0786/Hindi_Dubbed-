8673d17fd9b8c4d4791fa368ce4ba185c0cfdd1f
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-06-21 04:00:08 +0530

    Update OttsourcePlugin.kt

--- OttSource/src/main/kotlin/OttSource/OttsourcePlugin.kt
+++ OttSource/src/main/kotlin/OttSource/OttsourcePlugin.kt
@@ -13,7 +13,7 @@ open class OttSourcePlugin : Plugin() {
         NetflixMirrorStorage.init(context.applicationContext)
 
         // Pass context to providers
-        DisneyStudioProvider.context = context
+       
         NetflixMirrorProvider.context = context
         PrimeVideoMirrorProvider.context = context
         HotStarMirrorProvider.context = context
