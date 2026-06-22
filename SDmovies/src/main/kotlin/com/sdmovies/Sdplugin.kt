db6018bc9e99d83f9e70cacf27a417e3b16dbe8a
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-06-23 05:12:57 +0530

    Update Sdplugin.kt

--- SDmovies/src/main/kotlin/com/sdmovies/Sdplugin.kt
+++ SDmovies/src/main/kotlin/com/sdmovies/Sdplugin.kt
@@ -1,13 +1,13 @@
 package com.sdmovies
 
-import android.content.context
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class SDmoviesPlugin : Plugin() {
-    override fun load()(context: Context) {
+class Sdplugin : Plugin() {
+
+    override fun load(context: Context) {
         registerMainAPI(SDMoviesProvider())
-        
     }
 }
