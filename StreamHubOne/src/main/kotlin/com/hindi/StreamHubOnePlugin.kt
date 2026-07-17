25bb8c59df35e4bcde9254e94bd48b493efa7056
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-07-18 04:38:33 +0530

    Update StreamHubOnePlugin.kt

--- StreamHubOne/src/main/kotlin/com/hindi/StreamHubOnePlugin.kt
+++ StreamHubOne/src/main/kotlin/com/hindi/StreamHubOnePlugin.kt
@@ -1,11 +1,16 @@
 package com.hindi
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class StreamHubOnePlugin : Plugin() {
-    override fun load() {
+
+    override fun load(context: Context) {
+
+        AnimeCacheStorage.init(context.applicationContext)
+
         registerMainAPI(StreamHubOneProvider())
         registerMainAPI(Cwunchyroll())
     }
