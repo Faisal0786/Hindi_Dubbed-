598517925ce9d0ccb1d849873a6d2644311ec784
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-07-20 01:13:43 +0530

    Update build.gradle.kts

--- StreamHubOne/build.gradle.kts
+++ StreamHubOne/build.gradle.kts
@@ -4,21 +4,16 @@ version = 14
 
 android {
     defaultConfig {
-
         val properties = Properties()
-        properties.load(
-            project.rootProject
-                .file("local.properties")
-                .inputStream()
-        )
-
-        buildFeatures.buildConfig = true
-
-        buildConfigField(
-            "String",
-            "TMDB_KEY",
-            "\"${properties.getProperty("TMDB_KEY")}\""
-        )
+        properties.load(project.rootProject.file("local.properties").inputStream())
+        android.buildFeatures.buildConfig=true
+        buildConfigField("String", "SIMKL_API", "\"${properties.getProperty("SIMKL_API")}\"")
+        buildConfigField("String", "TMDB_KEY", "\"${properties.getProperty("TMDB_KEY")}\"")
+        buildConfigField("String", "CC_COOKIE", "\"${properties.getProperty("CC_COOKIE")}\"")
+        buildConfigField("String", "CASTLE_KEY", "\"${properties.getProperty("CASTLE_KEY")}\"")
+        buildConfigField("String", "MOVIEBLAST_TOKEN", "\"${properties.getProperty("MOVIEBLAST_TOKEN")}\"")
+        buildConfigField("String", "MOVIEBLAST_API", "\"${properties.getProperty("MOVIEBLAST_API")}\"")
+        buildConfigField("String", "MOVIEBLAST_KEY", "\"${properties.getProperty("MOVIEBLAST_KEY")}\"")
     }
 }
 
