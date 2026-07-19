b117884f5a02497bf290f72e0ebd4b278e331fe3
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-07-20 04:16:28 +0530

    Update Providers.kt

--- StreamHubOne/src/main/kotlin/com/hindi/providers/Providers.kt
+++ StreamHubOne/src/main/kotlin/com/hindi/providers/Providers.kt
@@ -33,7 +33,7 @@ import java.net.URL
 import java.net.URLEncoder
 import java.util.concurrent.ConcurrentHashMap
 
-import com.hindi.providers.Setting
+import com.hindi.providers.Settings
 
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
