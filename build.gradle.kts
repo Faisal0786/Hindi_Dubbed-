c2d12c16e29c2d9c0e29625f3916b988be2415dc
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-07-20 05:11:11 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,11 @@ subprojects {
             minSdk = 21
         }
 
+       buildFeatures {
+        buildConfig = true
+    }
+
+
       compileOptions {
             sourceCompatibility = JavaVersion.VERSION_1_8
             targetCompatibility = JavaVersion.VERSION_1_8
