e2d9f2b4aacb67f0f25fd8cce2da3be3a5a2a06f
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-07-24 04:13:09 +0530

    Update StreamHubOnePlugin.kt

--- StreamHubOne/src/main/kotlin/com/hindi/StreamHubOnePlugin.kt
+++ StreamHubOne/src/main/kotlin/com/hindi/StreamHubOnePlugin.kt
@@ -1,17 +1,36 @@
 package com.hindi
 
 import android.content.Context
+import com.lagradost.cloudstream3.MainActivity
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import com.hindi.providers.init
+import com.hindi.settings.Settings
+import kotlinx.coroutines.runBlocking
 
 @CloudstreamPlugin
 class StreamHubOnePlugin : Plugin() {
 
     override fun load(context: Context) {
 
+        // Initialize storage
         AnimeCacheStorage.init(context.applicationContext)
 
+        // Load dynamic API URLs
+        runBlocking { init() }
+
+        // Initialize provider settings
+        Settings.initSeenProviders()
+
+        // Register providers
         registerMainAPI(StreamHubOneProvider())
         registerMainAPI(Cwunchyroll())
+
+        // Open settings
+        this.openSettings = { ctx: Context ->
+            Settings.showSettingsDialog(ctx) {
+                MainActivity.reloadHomeEvent.invoke(true)
+            }
+        }
     }
 }
