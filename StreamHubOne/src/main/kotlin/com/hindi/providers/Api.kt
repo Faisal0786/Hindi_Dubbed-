fb148de5987563dda465255ff4a629ee08c711fd
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-07-29 02:22:05 +0530

    Update Api.kt

--- StreamHubOne/src/main/kotlin/com/hindi/providers/Api.kt
+++ StreamHubOne/src/main/kotlin/com/hindi/providers/Api.kt
@@ -5,7 +5,6 @@ import com.lagradost.cloudstream3.app
 import org.json.JSONObject
 import com.hindi.providers.BuildConfig
 
-
 // ── 1. Static APIs ──────────────────────────────────────────
 const val malsyncAPI = "https://api.malsync.moe"
 const val tokyoInsiderAPI = "https://www.tokyoinsider.com"
@@ -16,15 +15,15 @@ const val CASTLE_KEY = BuildConfig.CASTLE_KEY
 const val MOVIEBLAST_TOKEN = BuildConfig.MOVIEBLAST_TOKEN
 const val MOVIEBLAST_API = BuildConfig.MOVIEBLAST_API
 const val MOVIEBLAST_KEY = BuildConfig.MOVIEBLAST_KEY
+const val NETMIRROR_TOKEN = BuildConfig.NETMIRROR_TOKEN
 const val animepaheAPI = "https://animepahe.pw"
 const val allmovielandAPI = "https://allmovieland.one"
 const val anizoneAPI = "https://anizone.to"
-const val AllanimeAPI = "https://api.mkissa.net/api"
 const val PrimeSrcApi = "https://primesrc.me"
 const val asiaflixAPI = "https://asiaflix.net"
 const val dahmerMoviesAPI = "https://a.111477.xyz"
 const val hexaAPI = "https://theemoviedb.hexa.su"
-const val videasyAPI = "https://api.wingsdatabase.com"
+const val videasyAPI = "https://api.speedracelight.com"
 const val vidlinkAPI = "https://vidlink.pro"
 const val multiDecryptAPI = "https://enc-dec.app/api"
 const val animetoshoAPI = "https://feed.animetosho.xyz"
@@ -35,7 +34,6 @@ const val kissKhAPI = "https://kisskh.nl"
 const val vidupAPI = "https://vidup.to"
 const val bollywoodAPI = "https://tga-hd.api.hashhackers.com"
 const val bollywoodBaseAPI = "https://bollywood.eu.org"
-const val notorrentAPI = "https://addon-osvh.onrender.com"
 const val xpassAPI = "https://play.xpass.top"
 const val cinemacityAPI = "https://cinemacity.cc"
 const val akwamAPI = "https://akwam.it"
@@ -49,7 +47,6 @@ const val vidrockAPI = "https://vidrock.ru"
 const val animekizzAPI = "https://animekizz.live"
 const val vidfastProApi = "https://vidfast.vc"
 const val onetouchtvAPI = "https://api3.devcorp.me"
-const val playImdbAPI = "https://streamimdb.me"
 const val av1encodesAPI = "https://av1encodes.com"
 const val peachifyBaseAPI = "https://peachify.top"
 const val reanimeAPI = "https://reanime.to"
@@ -61,6 +58,7 @@ const val castleAPI = "https://api.hlowb.com"
 const val vidcoreAPI = "https://vidcore.net"
 const val anikageAPI = "https://anikage.cc"
 const val hdGharTvAPI = "https://hdghartv.cc"
+const val aninekoAPI = "https://anineko.to"
 const val torrentioAPI = "https://torrentio.strem.fun/limit=4"
 const val torrentsdbAPI = "https://torrentsdb.com/eyJsaW1pdCI6IjMiLCJkZWJyaWRvcHRpb25zIjpbIm5vZG93bmxvYWRsaW5rcyJdfQ=="
 
@@ -90,7 +88,6 @@ val hindMoviezAPI get() = api("hindmoviez")
 val hdmovie2API get() = api("hdmovie2")
 val nfmirrorAPI get() = api("nfmirror")
 val moviesdriveAPI get() = api("moviesdrive")
-val gojoBaseAPI get() = api("gojo_base")
 val vegamoviesAPI get() = api("vegamovies")
 val rogmoviesAPI get() = api("rogmovies")
 val uhdmoviesAPI get() = api("uhdmovies")
@@ -103,4 +100,4 @@ val m4ufreeAPI get() = api("m4ufree")
 val zinkmoviesAPI get() = api("zinkmovies")
 val animedaoAPI get() = api("animedao")
 val mlsbdAPI get() = api("mlsbd")
-const val net27API = "https://net27.cc"
+val fibwatchBaseUrl get() = api("fibwatch")
