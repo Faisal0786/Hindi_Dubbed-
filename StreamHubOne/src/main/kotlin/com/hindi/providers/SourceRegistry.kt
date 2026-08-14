11535f3d2e0416a53728292f3baa894145ac0e57
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-08-14 22:52:52 +0530

    Update SourceRegistry.kt

--- StreamHubOne/src/main/kotlin/com/hindi/providers/SourceRegistry.kt
+++ StreamHubOne/src/main/kotlin/com/hindi/providers/SourceRegistry.kt
@@ -49,6 +49,30 @@ object SourceRegistry {
             key = "p_vidrock", displayName = "Vidrock",
             executeStandard = { res, _, cb -> invokeVidrock(res.tmdbId, res.season, res.episode, cb) }
         ),
+
+        SourceProviderDef(
+            key = "p_cinemaos", 
+            displayName = "CinemaOS", 
+            category = ProviderCategory.HINDI,
+            executeStandard = { res, subCb, cb -> 
+                invokeCinemaos(res.title, res.tmdbId, res.imdbId, res.year, res.season, res.episode, subCb, cb) 
+            },
+            executeAnime = { res, subCb, cb -> 
+                invokeCinemaos(res.imdbTitle ?: res.title, res.tmdbId, res.imdbId, res.imdbYear ?: res.year, res.imdbSeason, res.imdbEpisode, subCb, cb) 
+            }
+        ),
+        SourceProviderDef(
+            key = "p_pvrmoviebox", 
+            displayName = "PvrMoviebox", 
+            category = ProviderCategory.HINDI,
+            executeStandard = { res, subCb, cb -> 
+                invokePvrMoviebox(res.title, res.tmdbId, res.imdbId, res.year, res.season, res.episode, subCb, cb) 
+            },
+            executeAnime = { res, subCb, cb -> 
+                invokePvrMoviebox(res.imdbTitle ?: res.title, res.tmdbId, res.imdbId, res.imdbYear ?: res.year, res.imdbSeason, res.imdbEpisode, subCb, cb) 
+            }
+        ),
+
         SourceProviderDef(
             key = "p_moviebox", displayName = "Moviebox", category = ProviderCategory.HINDI,
             executeStandard = { res, subCb, cb -> invokeMoviebox(res.title, res.season, res.episode, subCb, cb) },
