b52f461c0550163b8d8cbadf5ab5c9893c3fdbf7
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-08-15 00:57:58 +0530

    Update NewSourceRegistry.kt

--- StreamHubOne/src/main/kotlin/com/hindi/providers/NewProviders/NewSourceRegistry/NewSourceRegistry.kt
+++ StreamHubOne/src/main/kotlin/com/hindi/providers/NewProviders/NewSourceRegistry/NewSourceRegistry.kt
@@ -16,6 +16,14 @@ object NewSourceRegistry {
                 invokePvrMoviebox(res.imdbTitle ?: res.title, res.tmdbId, res.imdbId, res.imdbYear ?: res.year, res.imdbSeason, res.imdbEpisode, subCb, cb)
             }
         ),
+
+SourceProviderDef(
+            key = "p_anizone2", displayName = "Anizone 2",
+
+            executeAnime = { res, subCb, cb -> invokeAnizone2(res.originalTitle ?: res.title, res.episode, subCb, cb) },
+            executeMalSync = { data, subCb, cb -> if (data.origin == "imdb") invokeAnizone2(data.title, data.episode, subCb, cb) }
+        ),
+
         SourceProviderDef(
             key = "p_cinemaos",
             displayName = "CinemaOS",
