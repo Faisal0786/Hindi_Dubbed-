b64138cb6eca817ac4e87d32fec533c31d067959
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-08-15 00:56:48 +0530

    Update SourceRegistry.kt

--- StreamHubOne/src/main/kotlin/com/hindi/providers/SourceRegistry.kt
+++ StreamHubOne/src/main/kotlin/com/hindi/providers/SourceRegistry.kt
@@ -52,29 +52,8 @@ object SourceRegistry {
             executeStandard = { res, _, cb -> invokeVidrock(res.tmdbId, res.season, res.episode, cb) }
         ),
 
-        SourceProviderDef(
-            key = "p_cinemaos", 
-            displayName = "CinemaOS", 
-            category = ProviderCategory.HINDI,
-            executeStandard = { res, subCb, cb -> 
-                invokeCinemaos(res.title, res.tmdbId, res.imdbId, res.year, res.season, res.episode, subCb, cb) 
-            },
-            executeAnime = { res, subCb, cb -> 
-                invokeCinemaos(res.imdbTitle ?: res.title, res.tmdbId, res.imdbId, res.imdbYear ?: res.year, res.imdbSeason, res.imdbEpisode, subCb, cb) 
-            }
-        ),
-        SourceProviderDef(
-            key = "p_pvrmoviebox", 
-            displayName = "PvrMoviebox", 
-            category = ProviderCategory.HINDI,
-            executeStandard = { res, subCb, cb -> 
-                invokePvrMoviebox(res.title, res.tmdbId, res.imdbId, res.year, res.season, res.episode, subCb, cb) 
-            },
-            executeAnime = { res, subCb, cb -> 
-                invokePvrMoviebox(res.imdbTitle ?: res.title, res.tmdbId, res.imdbId, res.imdbYear ?: res.year, res.imdbSeason, res.imdbEpisode, subCb, cb) 
-            }
-        ),
-
+        
+       
         SourceProviderDef(
             key = "p_moviebox", displayName = "Moviebox", category = ProviderCategory.HINDI,
             executeStandard = { res, subCb, cb -> invokeMoviebox(res.title, res.season, res.episode, subCb, cb) },
@@ -336,12 +315,7 @@ object SourceRegistry {
             executeMalSync = { data, subCb, cb -> if (data.origin == "imdb") invokeTokyoInsider(data.title, data.episode, subCb, cb) }
         ),
 
-        SourceProviderDef(
-            key = "p_anizone2", displayName = "Anizone 2",
-
-            executeAnime = { res, subCb, cb -> invokeAnizone2(res.originalTitle ?: res.title, res.episode, subCb, cb) },
-            executeMalSync = { data, subCb, cb -> if (data.origin == "imdb") invokeAnizone2(data.title, data.episode, subCb, cb) }
-        ),
+        
 
         SourceProviderDef(
             key = "p_anizone", displayName = "Anizone",
