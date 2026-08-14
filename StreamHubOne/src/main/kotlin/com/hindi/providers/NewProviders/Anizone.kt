07544946b2df3869d691ca4e514acd9348bdf21d
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-08-15 02:02:44 +0530

    Update Anizone.kt

--- StreamHubOne/src/main/kotlin/com/hindi/providers/NewProviders/Anizone.kt
+++ StreamHubOne/src/main/kotlin/com/hindi/providers/NewProviders/Anizone.kt
@@ -1,7 +1,7 @@
 package com.hindi.providers.NewProviders
 
 
-import com.hindi.providers*
+import com.hindi.providers.*
 import com.hindi.providers.SourceProviders
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
