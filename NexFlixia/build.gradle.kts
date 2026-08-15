9f8dc30c8a6c4dedf0878c95240a9c99e2bc7e69
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-08-15 06:12:24 +0530

    Create build.gradle.kts

---
+++ NexFlixia/build.gradle.kts
@@ -0,0 +1,58 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+
+version = 1
+
+android {
+    defaultConfig {
+        val properties = Properties()
+        properties.load(project.rootProject.file("local.properties").inputStream())
+
+        android.buildFeatures.buildConfig = true
+
+        buildConfigField(
+            "String",
+            "SIMKL_API",
+            "\"${properties.getProperty("SIMKL_API")}\""
+        )
+
+        buildConfigField(
+            "String",
+            "TMDB_KEY",
+            "\"${properties.getProperty("TMDB_KEY")}\""
+        )
+
+        buildConfigField(
+            "String",
+            "CC_COOKIE",
+            "\"${properties.getProperty("CC_COOKIE")}\""
+        )
+
+        
+        
+
+        
+    }
+}
+
+cloudstream {
+    language = "hi"
+
+    description =
+        "NexFlixia - Multilingual all-rounder provider"
+
+    authors = listOf(
+        "Faisal"
+    )
+
+    status = 1
+
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime",
+        "AnimeMovie",
+        "AsianDrama"
+    )
+
+    iconUrl = ""
+}
