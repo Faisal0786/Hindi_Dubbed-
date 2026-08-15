22f0a4aaacf534fff9a960f911592f86b72b732f
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-08-15 06:29:35 +0530

    Update NexFlixiaApi.kt

--- NexFlixia/src/main/kotlin/com/multi/NexFlixia/NexFlixiaApi.kt
+++ NexFlixia/src/main/kotlin/com/multi/NexFlixia/NexFlixiaApi.kt
@@ -3,20 +3,40 @@ package com.multi.nexflixia
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.app
 
-
 class NexFlixiaApi(
     private val provider: MainAPI
 ) {
 
     companion object {
-        const val BASE_URL =
+        const val CINEMETA_BASE_URL =
             "https://v3-cinemeta.strem.io"
+
+        const val ANILIST_BASE_URL =
+            "https://graphql.anilist.co"
     }
 
-    suspend fun get(path: String): String? {
+    suspend fun get(
+        path: String
+    ): String? {
         return runCatching {
             provider.app
-                .get("$BASE_URL$path")
+                .get("$CINEMETA_BASE_URL$path")
+                .text
+        }.getOrNull()
+    }
+
+    suspend fun post(
+        url: String,
+        body: String,
+        headers: Map<String, String> = emptyMap()
+    ): String? {
+        return runCatching {
+            provider.app
+                .post(
+                    url = url,
+                    headers = headers,
+                    data = body
+                )
                 .text
         }.getOrNull()
     }
