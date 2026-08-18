e5c7dca7158f367717cc7a8ac96579f098dbc208
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-08-19 03:58:39 +0530

    Update NexFlixia.kt

--- NexFlixia/src/main/kotlin/com/multi/NexFlixia/NexFlixia.kt
+++ NexFlixia/src/main/kotlin/com/multi/NexFlixia/NexFlixia.kt
@@ -5,6 +5,7 @@ import com.lagradost.cloudstream3.LoadResponse.Companion.addImdbId
 import com.lagradost.cloudstream3.utils.AppUtils.parseJson
 import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
+import com.lagradost.cloudstream3.utils.ExtractorLink
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
