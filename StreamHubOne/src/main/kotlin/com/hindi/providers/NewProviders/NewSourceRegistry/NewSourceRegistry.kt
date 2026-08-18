c4d39fe3ef4e67171578144e70fa1ac199db3f46
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-08-18 17:24:14 +0530

    Update NewSourceRegistry.kt

--- StreamHubOne/src/main/kotlin/com/hindi/providers/NewProviders/NewSourceRegistry/NewSourceRegistry.kt
+++ StreamHubOne/src/main/kotlin/com/hindi/providers/NewProviders/NewSourceRegistry/NewSourceRegistry.kt
@@ -5,6 +5,10 @@ import com.hindi.providers.SourceProviders
 import com.lagradost.cloudstream3.ExtractorLink
 import com.lagradost.cloudstream3.SubtitleFile
 
+// Purane invoke functions ke liye imports
+import com.hindi.providers.NewProviders.*
+import com.hindi.providers.*
+
 enum class ProviderCategory {
     DEFAULT,
     HINDI
@@ -36,6 +40,50 @@ object NewSourceRegistry {
     val builtInProviders = listOf(
 
         NewSourceProviderDef(
+            key = "p_pvrmoviebox",
+            displayName = "PvrMoviebox",
+            category = ProviderCategory.HINDI,
+            executeStandard = { res, subCb, cb ->
+                invokePvrMoviebox(res.title, res.tmdbId, res.imdbId, res.year, res.season, res.episode, subCb, cb)
+            },
+            executeAnime = { res, subCb, cb ->
+                invokePvrMoviebox(res.imdbTitle ?: res.title, res.tmdbId, res.imdbId, res.imdbYear ?: res.year, res.imdbSeason, res.imdbEpisode, subCb, cb)
+            }
+        ),
+
+        NewSourceProviderDef(
+            key = "p_anizone2",
+            displayName = "Anizone 2",
+            executeAnime = { res, subCb, cb -> 
+                invokeAnizone2(res.originalTitle ?: res.title, res.episode, subCb, cb) 
+            }
+        ),
+
+        NewSourceProviderDef(
+            key = "p_cinemaos",
+            displayName = "CinemaOS",
+            category = ProviderCategory.HINDI,
+            executeStandard = { res, subCb, cb ->
+                invokeCinemaos(res.title, res.tmdbId, res.imdbId, res.year, res.season, res.episode, subCb, cb)
+            },
+            executeAnime = { res, subCb, cb ->
+                invokeCinemaos(res.imdbTitle ?: res.title, res.tmdbId, res.imdbId, res.imdbYear ?: res.year, res.imdbSeason, res.imdbEpisode, subCb, cb)
+            }
+        ),
+
+        NewSourceProviderDef(
+            key = "p_netflixmirror",
+            displayName = "Netflix Hindi",
+            category = ProviderCategory.HINDI,
+            executeStandard = { res, subCb, cb ->
+                invokeNetflixMirror(res.imdbId, res.title, res.season, res.episode, subCb, cb)
+            },
+            executeAnime = { res, subCb, cb ->
+                invokeNetflixMirror(res.imdbId, res.imdbTitle ?: res.title, res.imdbSeason, res.imdbEpisode, subCb, cb)
+            }
+        ),
+
+        NewSourceProviderDef(
             key = "p_anistream",
             displayName = "AniStream",
             executeAnime = { res, subCb, cb ->
@@ -48,6 +96,5 @@ object NewSourceRegistry {
                 )
             }
         )
-
     )
 }
