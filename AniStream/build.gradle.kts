15ad3fe0571dfdad497d152e0212fdbf6e30148c
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-08-18 14:59:47 +0530

    Create build.gradle.kts

---
+++ AniStream/build.gradle.kts
@@ -0,0 +1,65 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+version = 1
+
+android {
+    namespace = "com.anistream"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+
+        val properties = Properties()
+        val localPropertiesFile = project.rootProject.file("local.properties")
+        if (localPropertiesFile.exists()) {
+            properties.load(localPropertiesFile.inputStream())
+        }
+
+        android.buildFeatures.buildConfig = true
+
+        buildConfigField(
+            "String",
+            "TMDB_KEY",
+            "\"${properties.getProperty("TMDB_KEY", "")}\""
+        )
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = freeCompilerArgs + listOf(
+            "-opt-in=kotlin.RequiresOptIn"
+        )
+    }
+}
+
+cloudstream {
+    description = "Watch anime in HD with English Sub and Dub from AniStream"
+
+    authors = listOf(
+        "Faisal"
+    )
+
+    status = 1
+
+    tvTypes = listOf(
+        "Anime",
+        "AnimeMovie"
+    )
+
+    iconUrl = "https://anistream.one/og.png"
+}
+
+dependencies {
+    // Cloudstream core APIs gradle plugin ke through inject hoti hain
+}
