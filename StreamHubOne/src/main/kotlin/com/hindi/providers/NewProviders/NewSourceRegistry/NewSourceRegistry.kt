4b71e2b468fb66dca9c4f7a9458551e20d50f7bf
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-08-20 05:07:41 +0530

    Update NewSourceRegistry.kt

--- StreamHubOne/src/main/kotlin/com/hindi/providers/NewProviders/NewSourceRegistry/NewSourceRegistry.kt
+++ StreamHubOne/src/main/kotlin/com/hindi/providers/NewProviders/NewSourceRegistry/NewSourceRegistry.kt
@@ -38,20 +38,31 @@ object NewSourceRegistry {
         ),
 
         SourceProviderDef(
-            key = "p_reanime",
-            displayName = "Reanime",
-            executeAnime = { res, subCb, cb -> 
-                // isDub = false default rakha hai. Agar aapke 'res' object mein isDub ka option hai, 
-                // toh aap 'isDub = res.isDub' bhi use kar sakte hain.
-                invokeReanime(
-                    title = res.originalTitle ?: res.title, 
-                    episode = res.episode, 
-                    isDub = false, 
-                    subtitleCallback = subCb, 
-                    callback = cb
-                ) 
-            }
-        ),
+    key = "p_reanime",
+    displayName = "Reanime",
+    executeAnime = { res, subCb, cb ->
+
+        // Sub
+        invokeReanime(
+            title = res.originalTitle ?: res.title,
+            episode = res.episode,
+            anilistId = res.anilistId,
+            isDub = false,
+            subtitleCallback = subCb,
+            callback = cb
+        )
+
+        // Dub
+        invokeReanime(
+            title = res.originalTitle ?: res.title,
+            episode = res.episode,
+            anilistId = res.anilistId,
+            isDub = true,
+            subtitleCallback = subCb,
+            callback = cb
+        )
+    }
+),
 
         SourceProviderDef(
             key = "p_anistream",
