1855bd32beacfa8ae8f1efcc44c53df1816a19c7
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-08-20 05:30:54 +0530

    Update Cruwnchyroll.kt

--- StreamHubOne/src/main/kotlin/com/hindi/Cruwnchyroll.kt
+++ StreamHubOne/src/main/kotlin/com/hindi/Cruwnchyroll.kt
@@ -1241,7 +1241,7 @@ return newHomePageResponse(
                             invokeAnimes(malId, aniId, res.episode, seasonYear, animeSource, subtitleCallback, callback)
 invokeAniStream(aniId, res.episode, subtitleCallback, callback)
 
-invokeReanime(
+SourceProviders.invokeReanime(
     title = res.title, 
     episode = res.episode, 
     anilistId = aniId, 
