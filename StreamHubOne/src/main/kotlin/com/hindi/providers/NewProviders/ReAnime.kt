906ad2f441dbd069b05202699ad6bcdbb24273c3
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-08-21 01:40:26 +0530

    Update ReAnime.kt

--- StreamHubOne/src/main/kotlin/com/hindi/providers/NewProviders/ReAnime.kt
+++ StreamHubOne/src/main/kotlin/com/hindi/providers/NewProviders/ReAnime.kt
@@ -17,7 +17,7 @@ import org.json.JSONArray
 import org.json.JSONObject
 import java.net.URLEncoder
 
-suspend fun SourceProviders.invokeReanime(
+suspend fun SourceProviders.invokeReanimeB(
         title: String? = null,
         episode: Int? = null,
         anilistId: Int? = null,
