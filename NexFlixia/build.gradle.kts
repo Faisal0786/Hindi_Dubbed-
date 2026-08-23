0558dff2d39a40404c3d21a44c6c963bdd863ce2
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-08-24 01:46:17 +0530

    Update build.gradle.kts

--- NexFlixia/build.gradle.kts
+++ NexFlixia/build.gradle.kts
@@ -1,46 +1,41 @@
-import org.jetbrains.kotlin.konan.properties.Properties
+import java.util.Properties
 
 version = 5
 
 android {
     defaultConfig {
         val properties = Properties()
-        properties.load(project.rootProject.file("local.properties").inputStream())
+        val localPropsFile = project.rootProject.file("local.properties")
+        if (localPropsFile.exists()) {
+            properties.load(localPropsFile.inputStream())
+        }
 
-        buildFeatures.buildConfig = true // "android." yahan zaroori nahi hai andar
+        buildFeatures.buildConfig = true
 
         buildConfigField(
             "String",
             "SIMKL_API",
-            "\"${properties.getProperty("SIMKL_API")}\""
+            "\"${properties.getProperty("SIMKL_API", "")}\""
         )
 
         buildConfigField(
             "String",
             "TMDB_KEY",
-            "\"${properties.getProperty("TMDB_KEY")}\""
+            "\"${properties.getProperty("TMDB_KEY", "")}\""
         )
 
         buildConfigField(
             "String",
             "CC_COOKIE",
-            "\"${properties.getProperty("CC_COOKIE")}\""
+            "\"${properties.getProperty("CC_COOKIE", "")}\""
         )
-    }
-
-    // Bridge: Yahan SourceSets merge kar diya gaya hai
-    sourceSets {
-        getByName("main") {
-            java.srcDir(project(":StreamHubOne").file("src/main/kotlin"))
-        }
     }
 }
 
 cloudstream {
     language = "hi"
 
-    description =
-        "NexFlixia - Multilingual all-rounder provider"
+    description = "NexFlixia - Multilingual all-rounder provider"
 
     authors = listOf(
         "Faisal"
@@ -60,5 +55,5 @@ cloudstream {
 }
 
 dependencies {
-    compileOnly(project(":StreamHubOne"))
+    implementation(project(":StreamHubOne"))
 }
