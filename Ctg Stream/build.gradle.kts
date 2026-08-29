153ece54192108a8c6454bb81caf023133b2adc0
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-08-29 15:40:24 +0530

    Create build.gradle.kts

---
+++ Ctg Stream/build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+
+version = 1
+
+android {
+
+    defaultConfig {
+
+        val properties = Properties()
+
+        properties.load(
+            project.rootProject
+                .file("local.properties")
+                .inputStream()
+        )
+
+        android.buildFeatures.buildConfig = true
+
+        buildConfigField(
+            "String",
+            "TMDB_KEY",
+            "\"${properties.getProperty("TMDB_KEY")}\""
+        )
+    }
+}
+
+cloudstream {
+
+    description = "multi Language Provider"
+
+    authors = listOf(
+        "Faisal"
+    )
+
+    status = 1
+
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+        "AsianDrama",
+        "Anime",
+        "Torrent"
+    )
+
+    iconUrl = " "
+}
