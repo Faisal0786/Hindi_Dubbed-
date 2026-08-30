02bed92066ca527a5c1d6638bb5baac76c08cac9
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-08-31 00:08:31 +0530

    Create build.gradle.kts

---
+++ The Movie Flix/build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+
+version = 1
+
+android {
+    defaultConfig {
+        val properties = Properties()
+        properties.load(
+            project.rootProject
+                .file("local.properties")
+                .inputStream()
+        )
+        android.buildFeatures.buildConfig = true
+        buildConfigField(
+            "String",
+            "TMDB_KEY",
+            "\"${properties.getProperty("TMDB_KEY")}\""
+        )
+    }
+}
+
+cloudstream {
+    description = "TheMoviesFlix provider for Dual Audio, Bollywood and Hollywood Movies and TV Shows"
+    authors = listOf("Faisal")
+    status = 1
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+        "Anime"
+    )
+    iconUrl = "https://moviesflixi.com/favicon.ico"
+}
