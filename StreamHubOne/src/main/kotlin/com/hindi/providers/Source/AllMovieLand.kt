c6f262fb6c6014c952f7e8ecc03ec4eff6f5c028
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-09-05 02:45:19 +0530

    Update AllMovieLand.kt

--- StreamHubOne/src/main/kotlin/com/hindi/providers/Source/AllMovieLand.kt
+++ StreamHubOne/src/main/kotlin/com/hindi/providers/Source/AllMovieLand.kt
@@ -1,11 +1,44 @@
-package com.hindi.providers.sources
+package com.hindi.providers.Source
 
-import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.*
-import com.lagradost.cloudstream3.mvvm.safeAmap
-import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
 import com.hindi.providers.*
 import com.hindi.providers.SourceProviders
+
+// Cloudstream Core & Utils
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.mvvm.safeApiCall
+import com.lagradost.cloudstream3.network.CloudflareKiller
+import com.lagradost.cloudstream3.utils.*
+import android.webkit.CookieManager
+import com.lagradost.nicehttp.NiceResponse
+import com.lagradost.api.Log
+
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.MediaType.Companion.toMediaType
+
+// Jackson
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.lagradost.cloudstream3.utils.AppUtils.parseJson
+import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
+
+// Org JSON & Jsoup
+import org.json.JSONArray
+import org.json.JSONObject
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
+// Java Security, IO, & Encoding
+import java.io.IOException
+import java.nio.charset.StandardCharsets
+import java.security.SecureRandom
+
+// Java Net
+import java.net.URI
+import java.net.URL
+import java.net.URLEncoder
+import java.util.concurrent.ConcurrentHashMap
+
+
+
 
 suspend fun SourceProviders.invokeAllmovieland(
     id : String? = null,
