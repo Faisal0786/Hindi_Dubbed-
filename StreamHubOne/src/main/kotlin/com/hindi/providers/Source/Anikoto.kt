a7c1fa7e826b44bddced7ad55cab51aebefb82af
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-09-05 03:21:09 +0530

    Update Anikoto.kt

--- StreamHubOne/src/main/kotlin/com/hindi/providers/Source/Anikoto.kt
+++ StreamHubOne/src/main/kotlin/com/hindi/providers/Source/Anikoto.kt
@@ -41,64 +41,65 @@ import java.util.concurrent.ConcurrentHashMap
 
 
 suspend fun SourceProviders.invokeAnikoto(
-    title: String? = null,
-    year: Int? = null,
-    episode: Int? = null,
-    subtitleCallback: (SubtitleFile) -> Unit,
-    callback: (ExtractorLink) -> Unit,
-) {
-    val headers = mapOf(
-        "referer" to "$anikotoAPI/",
-        "x-requested-with" to "XMLHttpRequest"
-    )
+            title: String? = null,
+        year: Int? = null,
+        episode: Int? = null,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit,
+    ) {
+        val headers = mapOf(
+            "referer" to "$anikotoAPI/",
+            "x-requested-with" to "XMLHttpRequest"
+        )
 
-    val document = app.get(
-        "$anikotoAPI/filter?keyword=$title&type=&year%5B%5D=$year&ep_min=&ep_max=&sort=default"
-    ).document
+        val document = app.get(
+            "$anikotoAPI/filter?keyword=$title&type=&year%5B%5D=$year&ep_min=&ep_max=&sort=default"
+        ).document
 
-    val dataTip = document.selectFirst("div.tip.ani")?.attr("data-tip") ?: return
+        val dataTip = document.selectFirst("div.tip.ani")?.attr("data-tip") ?: return
 
-    Log.d("Anikoto", "dataTip: $dataTip")
+        Log.d("Anikoto", "dataTip: $dataTip")
 
-    val infoJson = app.get("$anikotoAPI/ajax/episode/list/$dataTip?vrf=", headers = headers).text
+        val infoJson = app.get("$anikotoAPI/ajax/episode/list/$dataTip?vrf=", headers = headers).text
 
-    Log.d("Anikoto", "infoJson: $infoJson")
+        Log.d("Anikoto", "infoJson: $infoJson")
 
-    val infoParsed = tryParseJson<AnikotoResponse>(infoJson) ?: return
-    val infoDocument = Jsoup.parse(infoParsed.result)
+        val infoParsed = tryParseJson<AnikotoResponse>(infoJson) ?: return
+        val infoDocument = Jsoup.parse(infoParsed.result)
 
-    val epAnchor = infoDocument.selectFirst("ul.ep-range li a[data-num='$episode']") ?: return
-    val dataIds = epAnchor.attr("data-ids")
+        val epAnchor = infoDocument.selectFirst("ul.ep-range li a[data-num='$episode']") ?: return
+        val dataIds = epAnchor.attr("data-ids")
 
-    Log.d("Anikoto", "dataIds: $dataIds")
+        Log.d("Anikoto", "dataIds: $dataIds")
 
-    // Fetch the server list HTML
-    val serversJson = app.get("$anikotoAPI/ajax/server/list?servers=$dataIds", headers = headers).text
+        // Fetch the server list HTML
+        val serversJson = app.get("$anikotoAPI/ajax/server/list?servers=$dataIds", headers = headers).text
 
-    Log.d("Anikoto", "headers = headers", serversJson) // keeping it clean
+        Log.d("Anikoto", "serversJson: $serversJson")
 
-    val serversParsed = tryParseJson<AnikotoResponse>(serversJson) ?: return
-    val serversDocument = Jsoup.parse(serversParsed.result)
+        val serversParsed = tryParseJson<AnikotoResponse>(serversJson) ?: return
+        val serversDocument = Jsoup.parse(serversParsed.result)
 
-    val serverTypes = serversDocument.select("div.servers div.type")
+        val serverTypes = serversDocument.select("div.servers div.type")
 
-    serverTypes.safeAmap { serverType ->
-        val type = serverType.attr("data-type").capitalizeServer()
+        serverTypes.safeAmap { serverType ->
+            val type = serverType.attr("data-type").capitalizeServer()
 
-        val serverList = serverType.select("ul li")
-        serverList.safeAmap { server ->
-            val serverName = server.text().trim()
-            val linkId = server.attr("data-link-id")
+            val serverList = serverType.select("ul li")
+            serverList.safeAmap { server ->
+                val serverName = server.text().trim()
+                val linkId = server.attr("data-link-id")
 
-            Log.d("Anikoto", "linkId: $linkId")
+                Log.d("Anikoto", "linkId: $linkId")
 
-            val serverResponseJson = app.get("$anikotoAPI/ajax/server?get=$linkId", headers = headers).text
-            val serverResponse = tryParseJson<AnikotoServerResponse>(serverResponseJson) ?: return@safeAmap
-            val embedUrl = serverResponse.result?.url ?: return@safeAmap
+                val serverResponseJson = app.get("$anikotoAPI/ajax/server?get=$linkId", headers = headers).text
+                val serverResponse = tryParseJson<AnikotoServerResponse>(serverResponseJson) ?: return@safeAmap
+                val embedUrl = serverResponse.result?.url ?: return@safeAmap
 
-            Log.d("Anikoto", "Extracted embed URL: $embedUrl")
+                Log.d("Anikoto", "Extracted embed URL: $embedUrl")
 
-            loadCustomExtractor("Anikoto[$type]", embedUrl, "$anikotoAPI/", subtitleCallback, callback)
+                loadCustomExtractor("Anikoto[$type]", embedUrl, "$anikotoAPI/", subtitleCallback, callback)
+
+            }
         }
     }
-}
