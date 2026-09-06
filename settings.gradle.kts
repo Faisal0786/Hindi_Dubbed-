8211af76061c48d456f0910a02d1a49981cf67e3
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-09-06 17:22:56 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ rootProject.name = "CloudstreamPlugins"
 // This file sets what projects are included.
 // All new projects should get automatically included unless specified in the "disabled" variable.
 
-val disabled = listOf("NexFlixia","StreamImdb") 
+val disabled = listOf("NexFlixia","StreamImdb","PlusBox") 
 
 
 File(rootDir, ".").eachDir { dir ->
