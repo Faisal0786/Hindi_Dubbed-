a78c52edeb1fb3f23d538d5e3f4bed6fbf6e3e3d
Faisal <39812782+Faisal0786@users.noreply.github.com>
2026-09-14 02:07:30 +0530

    Update Plugin.kt

--- Anime/src/main/kotlin/com/anime/Plugin.kt
+++ Anime/src/main/kotlin/com/anime/Plugin.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class KitsuAnimePlugin : Plugin() {
+class KitsuProviderPlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(KitsuAnimeProvider())
     }
